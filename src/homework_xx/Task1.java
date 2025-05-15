40037647e419a75b4c9d1fb60937ed520b918a80
SergeyB <sergiibugaienko@gmail.com>
2025-05-15 19:37:00 +0200

    hw_xx add

---
+++ src/homework_xx/Task1.java
@@ -0,0 +1,12 @@
+package homework_xx;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 15.05.2025
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/homework_xx/Task2.java
@@ -0,0 +1,16 @@
+package homework_xx;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 15.05.2025
+ */
+
+/*
+Условие задачи
+ */
+
+public class Task2 {
+    public static void main(String[] args) {
+
+    }
+}
